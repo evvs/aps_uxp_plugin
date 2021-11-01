563c25f58c18fa7de45a0c2e1339b9d244f43014
evvs <va.evgeny@gmail.com>
2021-11-01 18:03:28 +0300

    absolute path

--- src/components/ActionButton/index.jsx
+++ src/components/ActionButton/index.jsx
@@ -1,58 +1,68 @@
 import React from "react";
-import "./styles.css"
-
+import "./styles.css";
 
 const uxp = require("uxp");
 const fs = uxp.storage.localFileSystem;
 
-/*const batchPlay = require("photoshop").action.batchPlay;
+const batchPlay = require("photoshop").action.batchPlay;
 
 async function ScriptRun(jsxFile) {
-   let pluginFolder = await fs.getPluginFolder()
-   try {
-      let jsxFileObject = await pluginFolder.getEntry(jsxFile);
-      var filetoken = await fs.createSessionToken(jsxFileObject);
-   } catch (e) {
-      app.showAlert("File Can't be found!");
-   }
-   return await batchPlay(
-      [
-         {
-            "_obj": "AdobeScriptAutomation Scripts",
-            "javaScript": {
-               "_path": filetoken,
-               "_kind": "local"
-            },
-            "javaScriptMessage": "JSM",
-            "_isCommand": true,
-            "_options": {
+   console.log(jsxFile)
+  let pluginFolder = await fs.getTemporaryFolder();
+  try {
+    let jsxFileObject = await pluginFolder.getEntry(jsxFile);
+    var filetoken = await fs.createSessionToken(jsxFileObject);
+  } catch (e) {
+    app.showAlert("File Can't be found!");
+  }
+  return await batchPlay(
+    [
+      {
+        _obj: "AdobeScriptAutomation Scripts",
+        javaScript: {
+          _path: filetoken,
+          _kind: "local",
+        },
+        javaScriptMessage: "JSM",
+        _isCommand: true,
+        _options: {
+          dialogOptions: "dontDisplay",
+        },
+      },
+    ],
+    {
+      synchronousExecution: false,
+      modalBehavior: "fail",
+    }
+  );
+}
 
-               "dialogOptions": "dontDisplay"
+const createFile = async () => {
+  const folder = await fs.getTemporaryFolder();
+  const file = await folder.createFile("action.jsx", {
+    overwrite: true,
+  });
+  await file.write(`$.evalFile('C:/Program Files/Adobe/Adobe Photoshop 2021/Presets/Scripts/APs_Scripts/APs_RETOUCH_set_ru/_APsRETOUCH_FirmLib_ru.jsm')
+$.evalFile('C:/Program Files/Adobe/Adobe Photoshop 2021/Presets/Scripts/APs_Scripts/APs_RETOUCH_set_ru/_APsRETOUCH_KeywordsLib_ru.jsm')
+   
+do_Action('APs_RETOUCH_Profi', 'Pseudo HDR');`);
 
-            },
-         }
-      ], {
-      "synchronousExecution": false,
-      "modalBehavior": "fail"
-   });
+   return 'action.jsx'
+};
 
-} */
-
-
-
-const createFile = async() => {
-   console.log('file!!!!')
-   const folder = await fs.getTemporaryFolder();
-   const file = await folder.createFile("test.jsx", {
-      overwrite: true
-    });
-   await file.write(`qwertt`)
+const runAction = async() => {
+   const path = await createFile()
+   await ScriptRun(path)
 }
 
 export default ({ name, description, color, standartActions, expandedEctions }) => {
   return (
-      <sp-action-button title={description} class="action-btn"
-      onClick={() => createFile()}
-      >{name}</sp-action-button>
+    <sp-action-button
+      title={description}
+      class="action-btn"
+      onClick={() => runAction()}
+    >
+      {name}
+    </sp-action-button>
   );
 };
