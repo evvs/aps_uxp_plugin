cb4c3febfd6f660b32f69b66e211533d46b65640
Evgeny Valyaev <45902991+evvs@users.noreply.github.com>
2021-12-01 05:49:21 +0300

    top & bottom menus events

--- src/components/BottomMenu/index.jsx
+++ src/components/BottomMenu/index.jsx
@@ -1,46 +1,94 @@
 import React from "react";
+import runAction from "../../utils/runAction";
 
+import "./styles.css";
 
-import "./styles.css"
+export default ({ state }) => {
+  const standartExpandedLibraryOnClick = (action) => {
+    if (state.modes.expanded) {
+      runAction({
+        setName: "APs_RETOUCH_Expanded",
+        actionName: action,
+      });
+    } else {
+      runAction({
+        setName: "APs_RETOUCH_Standart",
+        actionName: action,
+      });
+    }
+  };
 
-export default () => {
+  const retouchLibraryOnClick = (action) => {
+    runAction({
+      setName: "APs_RETOUCH_Library",
+      actionName: action,
+    });
+  };
 
-    return <div className="bottom-menu">
-        <sp-action-button>
-            <img src="./icons/eraser.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/brush.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/brushMix.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/stamp.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/analyse.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/defects.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/divide.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/difference.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/services_panel_16x16.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/mergeLayers.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/saveAutoIndex.svg" />
-        </sp-action-button>
-        <sp-action-button>
-            <img src="./icons/next.svg" />
-        </sp-action-button>
+  const retouchServicesLibraryOnClick = (action) => {
+    runAction({
+      setName: "APs_RETOUCH_Services",
+      actionName: action,
+    });
+  };
+
+  return (
+    <div className="bottom-menu">
+      <sp-action-button onClick={() => standartExpandedLibraryOnClick("Ластик")}>
+        <img src="./icons/eraser.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => standartExpandedLibraryOnClick("Кисть")}>
+        <img src="./icons/brush.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => retouchLibraryOnClick("TOOL_Brush Mixer")}>
+        <img src="./icons/brushMix.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => retouchLibraryOnClick("TOOL_Stamp")}>
+        <img src="./icons/stamp.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => standartExpandedLibraryOnClick("Анализ               F3")}>
+        <img src="./icons/analyse.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => retouchLibraryOnClick("SERVICE_Defects_Show_Solarization")}>
+        <img src="./icons/defects.svg" />
+      </sp-action-button>
+
+      <sp-action-button
+        onClick={() => retouchLibraryOnClick("SERVICE_Decomposition_Color-Luminosity")}
+      >
+        <img src="./icons/divide.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => retouchLibraryOnClick("СЕРВИС_Разница")}>
+        <img src="./icons/difference.svg" />
+      </sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          standartExpandedLibraryOnClick(
+            state.modes.expanded ? "Сервисы      (Guru)" : "Сервисы      (Profi)"
+          );
+        }}
+      >
+        <img src="./icons/services_panel_16x16.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => standartExpandedLibraryOnClick("Соединить слои")}>
+        <img src="./icons/mergeLayers.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => retouchLibraryOnClick("DOCUMENT_Save_PSD_Autoindex")}>
+        <img src="./icons/saveAutoIndex.svg" />
+      </sp-action-button>
+
+      <sp-action-button onClick={() => standartExpandedLibraryOnClick("СЛЕДУЮЩИЙ")}>
+        <img src="./icons/next.svg" />
+      </sp-action-button>
     </div>
-}
+  );
+};
--- src/components/TopMenu/index.jsx
+++ src/components/TopMenu/index.jsx
@@ -8,55 +8,120 @@ import "./styles.css";
 export default ({ state, dispatch }) => {
   const [isVisibleDropDown, setisVisibleDropDown] = useState(false);
 
+  const standartExpandedLibraryOnClick = (action) => {
+    if (state.modes.expanded) {
+      runAction({
+        setName: "APs_RETOUCH_Expanded",
+        actionName: action,
+      });
+    } else {
+      runAction({
+        setName: "APs_RETOUCH_Standart",
+        actionName: action,
+      });
+    }
+  };
+
+  const retouchLibraryOnClick = (action) => {
+    runAction({
+      setName: "APs_RETOUCH_Library",
+      actionName: action,
+    });
+  };
+
+  const retouchServicesLibraryOnClick = (action) => {
+    runAction({
+      setName: "APs_RETOUCH_Services",
+      actionName: action,
+    });
+  };
+
   return (
     <div className="top-menu">
-      <sp-action-button>
-        <img src="./icons/help.svg" className="filter-green" />
-      </sp-action-button>
       <sp-action-button
         onClick={() => {
-          runAction({
-            setName: "APs_RETOUCH_Guru",
-            actionName: "До / После         F2",
-          });
+          standartExpandedLibraryOnClick("ПОМОЩЬ          F4");
+        }}
+      >
+        <img src="./icons/help.svg" className="filter-green" />
+      </sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          standartExpandedLibraryOnClick("До / После         F2");
         }}
       >
         <img src="./icons/beforeAfter.svg" />
       </sp-action-button>
+
       <sp-action-button
         onClick={() => {
-          runAction({
-            setName: "APs_RETOUCH_Library",
-            actionName: "SERVICE_2 windows",
-          });
+          retouchLibraryOnClick("SERVICE_2 windows");
         }}
       >
         <img src="./icons/two_windows_15x16.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          retouchLibraryOnClick("LAYERS_Shot visible_Top of the document");
+        }}
+      >
         <img src="./icons/snapshot.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          retouchLibraryOnClick("Smart-object_Download RAW source");
+        }}
+      >
         <img src="./icons/raw.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          retouchServicesLibraryOnClick("Контейнер");
+        }}
+      >
         <img src="./icons/container.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          retouchServicesLibraryOnClick("Слои");
+        }}
+      >
         <img src="./icons/layers.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          retouchServicesLibraryOnClick("Документ");
+        }}
+      >
         <img src="./icons/document.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          retouchServicesLibraryOnClick("Папка");
+        }}
+      >
         <img src="./icons/folders_16x15.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button
+        onClick={() => {
+          retouchServicesLibraryOnClick("Пресет");
+        }}
+      >
         <img src="./icons/preset.svg" />
       </sp-action-button>
-      <sp-action-button>
+
+      <sp-action-button onClick={() => standartExpandedLibraryOnClick("ИЗБРАННОЕ      F5")}>
         <img src="./icons/favorites.svg" />
       </sp-action-button>
+
       <sp-action-button onClick={() => setisVisibleDropDown(!isVisibleDropDown)}>
         <img src="./icons/settings.svg" />
       </sp-action-button>
