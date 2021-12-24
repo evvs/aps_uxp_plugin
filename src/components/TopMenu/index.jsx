8df561260e3f441290c1fa578293c57cc2653b1d
Evgeny Valyaev <45902991+evvs@users.noreply.github.com>
2021-12-24 11:04:59 +0300

    prebuild

--- src/components/TopMenu/index.jsx
+++ src/components/TopMenu/index.jsx
@@ -15,28 +15,127 @@ import Folder from "../../../plugin/icons/Folder";
 import Settings from "../../../plugin/icons/Settings";
 import "./styles.css";
 
-const btnsHints = {
-  helpBtn: "«ПОМОЩЬ» (F4): Подсказки по работе с текущим инструментом и его слоями.",
-  beforeAfterBtn: "сервис: До/После (F2)",
-  objectsBtn:
-    "Объекты- (F1): Выделите объект и выполните эту команду чтобы удалить объект «с учётом содержимого»",
-  twoWindowsBtn: "Два окна. Удобно использовать для контроля ретуши",
-  snapshotBtn: "Слой-снимок текущего состояния документа.",
-  rawBtn:
-    "Добавить вверху документа слой смарт-объект с исходными Raw данными. В папке текущего документа (учитывая все под-папки) ищется одноимённый документ любого формата Raw. Если не находит - создаётся смарт-объект со снимком слоёв.",
-  containerBtn: "Добавить тематический «Контейнер»",
-  layersBtn: "сервис «Слои»",
-  documentBtn: "сервис «Документ»",
-  folderBtn: "сервис «Открыть папку»",
-  presetBtn: "сервис: «Сохранить в пресет»",
-  favoritesBtn: "Системная под-панель «ИЗБРАННОЕ» (F5)",
-  settingsBtn: "Настройки панели",
+const standartExpandedLibraryOnClick = (action, state) => {
+  if (state.modes.expanded) {
+    runAction({
+      setName: "APs_RETOUCH_Expanded",
+      actionName: action,
+    });
+  } else {
+    runAction({
+      setName: "APs_RETOUCH_Standart",
+      actionName: action,
+    });
+  }
 };
 
-const changeTopMenuHint = (hint) => ({
+const retouchLibraryOnClick = (action, state) => {
+  runAction({
+    setName: "APs_RETOUCH_Library",
+    actionName: action,
+  });
+};
+
+const retouchServicesLibraryOnClick = (action, state) => {
+  runAction({
+    setName: "APs_RETOUCH_Services",
+    actionName: action,
+  });
+};
+
+const changeTopMenuHint = (hint, state) => ({
   type: "changeTopMenuHint",
   payload: hint,
 });
+
+const btns = [
+  {
+    hint: "«ПОМОЩЬ» (F4): Подсказки по работе с текущим инструментом и его слоями.",
+    id: "helpBtn",
+    clickHandler: (state) => standartExpandedLibraryOnClick("ПОМОЩЬ          F4",  state),
+    icon: <Help />,
+  },
+  {
+    hint: "сервис: До/После (F2)",
+    id: "beforeAfterBtn",
+    clickHandler: (state) => standartExpandedLibraryOnClick("До / После         F2", state),
+    icon: <BeforeAfter />,
+  },
+  {
+    hint: "Объекты- (F1): Выделите объект и выполните эту команду чтобы удалить объект «с учётом содержимого»",
+    id: "objectsBtn",
+    clickHandler: (state) => standartExpandedLibraryOnClick("APs_RETOUCH_Services | Объекты-", state),
+    icon: <Objects />,
+  },
+  {
+    hint: "Два окна. Удобно использовать для контроля ретуши",
+    id: "twoWindowsBtn",
+    clickHandler: (state) => retouchLibraryOnClick("SERVICE_2 windows", state),
+    icon: <TwoWindows />,
+  },
+  {
+    hint: "Слой-снимок текущего состояния документа.",
+    id: "snapshotBtn",
+    clickHandler: (state ) => retouchLibraryOnClick("LAYERS_Shot visible_Top of the document", state),
+    icon: <Snapshot />,
+  },
+  {
+    hint: "Добавить вверху документа слой смарт-объект с исходными Raw данными. В папке текущего документа (учитывая все под-папки) ищется одноимённый документ любого формата Raw. Если не находит - создаётся смарт-объект со снимком слоёв.",
+    id: "rawBtn",
+    clickHandler: (state) => retouchLibraryOnClick("Smart-object_Download RAW source", state),
+    icon: <Raw />,
+  },
+  //   { hint: "Добавить тематический «Контейнер»",
+  //   id: "containerBtn",
+  //   clickHandler: () =>
+  // }
+
+  {
+    hint: "сервис «Слои»",
+    id: "layersBtn",
+    clickHandler: (state) => retouchServicesLibraryOnClick("Слои", state),
+    icon: <Layers />,
+  },
+  {
+    hint: "сервис «Документ»",
+    id: "documentBtn",
+    clickHandler: (state) => retouchServicesLibraryOnClick("Документ", state),
+    icon: <Document />,
+  },
+  {
+    hint: "сервис «Открыть папку»",
+    id: "folderBtn",
+    clickHandler: (state) => retouchServicesLibraryOnClick("Папка", state),
+    icon: <Folder />,
+  },
+  {
+    hint: "сервис: «Сохранить в пресет»",
+    id: "presetBtn",
+    clickHandler: (state) => retouchServicesLibraryOnClick("Пресет", state),
+    icon: <Preset />,
+  },
+  {
+    hint: "Системная под-панель «ИЗБРАННОЕ» (F5)",
+    id: "favoritesBtn",
+    clickHandler: (state) => standartExpandedLibraryOnClick("ИЗБРАННОЕ      F5", state),
+    icon: <Favorites />,
+  },
+];
+
+const TopButton = ({ id, hint, clickHandler, icon, state, dispatch }) => {
+  const onChangeHintEvent = (hint) => {
+    dispatch(changeTopMenuHint(hint));
+  };
+  return (
+    <sp-action-button
+      onClick={() => clickHandler(state)}
+      onMouseEnter={() => state.modes.about && onChangeHintEvent(hint)}
+      onMouseLeave={() => onChangeHintEvent("")}
+    >
+      {icon}
+    </sp-action-button>
+  );
+};
 
 export default forwardRef(({ state, dispatch }, ref) => {
   const [isVisibleDropDown, setisVisibleDropDown] = useState(false);
@@ -62,147 +161,23 @@ export default forwardRef(({ state, dispatch }, ref) => {
     dispatch(changeTopMenuHint(hint));
   };
 
-  const standartExpandedLibraryOnClick = (action) => {
-    if (state.modes.expanded) {
-      runAction({
-        setName: "APs_RETOUCH_Expanded",
-        actionName: action,
-      });
-    } else {
-      runAction({
-        setName: "APs_RETOUCH_Standart",
-        actionName: action,
-      });
-    }
-  };
-
-  const retouchLibraryOnClick = (action) => {
-    runAction({
-      setName: "APs_RETOUCH_Library",
-      actionName: action,
-    });
-  };
-
-  const retouchServicesLibraryOnClick = (action) => {
-    runAction({
-      setName: "APs_RETOUCH_Services",
-      actionName: action,
-    });
-  };
-
   return (
     <div className="top-menu" ref={ref}>
-      <sp-action-button
-        onClick={() => {
-          standartExpandedLibraryOnClick("ПОМОЩЬ          F4");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.helpBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Help />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          standartExpandedLibraryOnClick("До / После         F2");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.beforeAfterBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <BeforeAfter />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => standartExpandedLibraryOnClick("APs_RETOUCH_Services | Объекты-")}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.objectsBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Objects />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          retouchLibraryOnClick("SERVICE_2 windows");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.twoWindowsBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <TwoWindows />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          retouchLibraryOnClick("LAYERS_Shot visible_Top of the document");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.snapshotBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Snapshot />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          retouchLibraryOnClick("Smart-object_Download RAW source");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.rawBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Raw />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          retouchServicesLibraryOnClick("Слои");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.layersBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Layers />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          retouchServicesLibraryOnClick("Документ");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.documentBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Document />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          retouchServicesLibraryOnClick("Папка");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.folderBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Folder />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => {
-          retouchServicesLibraryOnClick("Пресет");
-        }}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.presetBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Preset />
-      </sp-action-button>
-
-      <sp-action-button
-        onClick={() => standartExpandedLibraryOnClick("ИЗБРАННОЕ      F5")}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.favoritesBtn)}
-        onMouseLeave={() => onChangeHintEvent("")}
-      >
-        <Favorites />
-      </sp-action-button>
-
+      {btns.map(({ id, hint, clickHandler, icon }) => (
+        <TopButton
+          id={id}
+          hint={hint}
+          clickHandler={clickHandler}
+          icon={icon}
+          state={state}
+          dispatch={dispatch}
+        />
+      ))}
       <sp-action-button
         onClick={() => setClick((prev) => prev + 1)}
         onDoubleClick={() => setClick((prev) => prev + 1)}
         class={state.modes.expanded ? "red-btn" : ""}
-        onMouseEnter={() => state.modes.about && onChangeHintEvent(btnsHints.settingsBtn)}
+        onMouseEnter={() => state.modes.about && onChangeHintEvent("Настройки панели")}
         onMouseLeave={() => onChangeHintEvent("")}
       >
         <Settings />
